55d3fa0e1024248fc366e105ffd03a1d05c1d602
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-09 18:35:19 +0100

    null error file 6

--- print_string.c
+++ print_string.c
@@ -1,22 +1,37 @@
 #include "main.h"
 /**
- * 
+ *
  * b njr
- * 
- * 
-*/
+ *
+ *
+ */
 int print_string(va_list p)
 {
     char *s;
-    int ls ,j, i = 0;
+    int ls, j, i = 0;
 
-    s = va_arg(p, char*);
-    ls = strlen(s);
-	for (j = 1; j <=ls ; j++)
-	{
-		_putchar(*s);
-		s++;
-        i++;
-	}
-    return(i);
+    s = va_arg(p, char *);
+    if (s == NULL)
+    {
+
+        s = "(null)";
+        ls = strlen(s);
+        for (j = 1; j <= ls; j++)
+        {
+            _putchar(*s);
+            s++;
+            i++;
+        }
+    }
+    else
+    {
+        ls = strlen(s);
+        for (j = 1; j <= ls; j++)
+        {
+            _putchar(*s);
+            s++;
+            i++;
+        }
+    }
+    return (i);
 }
