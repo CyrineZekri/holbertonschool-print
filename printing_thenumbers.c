7c5c095c37604b07fe3d396c6c9d25b98a3af29f
Aminekham <lkham0508@gmail.com>
2022-11-11 22:14:17 +0100

    the numbers full function

--- printing_thenumbers.c
+++ printing_thenumbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
-int printing_number(va_list liste)
+long int printing_number(va_list liste)
 {
-    int n, c = 0 ,f = 0, x = 1, i = 0, k;
+    long int n, c = 0 ,f = 0, k;
 
-    n = va_arg(liste, int);
+    n = va_arg(liste,int);
     if (n < 0)
     {
         _putchar('-');
@@ -16,22 +16,6 @@ int printing_number(va_list liste)
         k = k / 10;
         c++;
     }
-    while (i < c)
-    {
-        x = x * 10;
-        i++;
-    }
-    while (n >= 10)
-    {
-        _putchar(n / x + '0');
-        n = n % x;
-        x = x / 10;
-        f++;
-    }
-    if (n < 10)
-    {
-        _putchar(n + '0');
-        f++;
-    }
+    f = numbers (n, c, f) + 1;
     return(f);
 }
