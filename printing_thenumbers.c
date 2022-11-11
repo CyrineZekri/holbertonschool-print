1b2c0f5fe9d45472d238f42709b78c33a2f870dd
Aminekham <lkham0508@gmail.com>
2022-11-11 12:20:16 +0100

    last update for d and i

--- printing_thenumbers.c
+++ printing_thenumbers.c
@@ -7,17 +7,17 @@ int printing_number(va_list liste)
     {
         _putchar('-');
         n = (-1) * n;
-        while (n >= 10)
-        {
-            n = n / 10;
-            c++;
-        }
-        x = 10 ^ c;
-        while (n >= 10)
-        {
-            _putchar(n % x + '0');
-            n = n / x;
-            x = x / 10;
-        }
+    }
+    while (n >= 10)
+    {
+        n = n / 10;
+        c++;
+    }
+    x = 10 ^ c;
+    while (n >= 10)
+    {
+        _putchar(n % x + '0');
+        n = n / x;
+        x = x / 10;
     }
 }
