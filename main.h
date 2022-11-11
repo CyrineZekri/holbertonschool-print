0f6a0f4cc57334205aa33a2281c1310ddcc2384a
Aminekham <lkham0508@gmail.com>
2022-11-11 22:17:36 +0100

    the full headers

--- main.h
+++ main.h
@@ -10,7 +10,8 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int normal_process(const char* format, int l,va_list liste);
 int process_function(va_list list, const char *format, int i, int f);
-int printing_number(va_list liste);
+long int printing_number(va_list liste);
+int numbers(long int n, long int c, int f);
 void print_char(va_list p);
 int print_string(va_list p);
 #endif
