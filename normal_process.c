3284d311f509e58440b772c1374824caa6258250
Aminekham <lkham0508@gmail.com>
2022-11-11 11:43:49 +0100

    the whole process

---
+++ normal_process.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int normal_process(const char* format, int l,va_list liste)
+{
+	int n, i;
+	n = strlen(format);
+	for (i = 0; i < n; i++)
+	{
+		if (*(format + i) != '%')
+		{
+			_putchar(*(format + i));
+			l++;
+		}
+		else if (*(format + i) == '%')
+		{
+			if (*(format + i + 1) == 's' || *(format + i + 1) == 'c')
+			{
+				l = l + process_function(liste, format, i, 0);
+				i = i + 1;
+			}
+			else if (*(format + i + 1) == 'd' || *(format + i + 1) == 'i')
+			{
+				l = l + printing_numbers(liste);
+				i = i + 1;
+			}
+		}
+	}
+	return(l);
+}
