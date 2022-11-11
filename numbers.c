b177b7cdd1d543021d65ed580c7acad7ea91bd03
Aminekham <lkham0508@gmail.com>
2022-11-11 22:15:35 +0100

    fixing the function

---
+++ numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * 
+ * 
+*/
+int numbers(long int n, long int c, int f)
+{
+    int i = 0; 
+    long int x = 1;
+    while (i < c)
+    {
+        x = x * 10;
+        i++;
+    }
+    i = 0;
+    while (i < c)
+    {
+        _putchar(n / x + '0');
+        n = n % x;
+        x = x / 10;
+        f++;
+        i++;
+    }
+    if (n < 10)
+    {
+        _putchar(n + '0');
+    }
+    return(f);
+}
