b23500a8d3284a861d1be7052f47b153c9088f10
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-12 22:12:47 +0100

    betty for main.h

--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 #include <limits.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
-int normal_process(const char* format, int l,va_list liste);
+int normal_process(const char *format, int l, va_list liste);
 int process_function(va_list list, const char *format, int i, int f);
 long int printing_number(va_list liste);
 int numbers(long int n, long int c, int f);
